cb723a9418e8febca5930a18398a433090ebc5b4
Guruguvi <116004662+Guruguvi@users.noreply.github.com>
2022-10-26 10:31:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// 1. How to campare two JSON have same properties without order ?
+
+var obj1 = { name: "Person 1", age:5 };
+var obj2 = { age:5, name: "Person 1" };
+
+ var JSON = true;
+ if(Object.keys(obj1).length==Object.keys(obj2).length){
+    for(let key in obj1){
+        if(obj1[key]==obj2[key]){
+            continue;
+        }
+        else{
+            JSON = false;
+            break;
+        }
+    }
+ }
+ else{
+   JSON = false;
+ }
+ console.log(JSON);
